959de737dc4cce9c7e19c98610a67b219c5e8048
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-09-08 16:38:53 +0300

    Updates

---
+++ precache-manifest.113462765d1d67409a6f492d9c41fb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fd4f65d8f20898197c357f5c87731f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "6584520b93d2ec14e131",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d07e29699b0f9bd50a68",
+    "url": "/todolist/static/js/2.bb1b8250.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.bb1b8250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6584520b93d2ec14e131",
+    "url": "/todolist/static/js/main.81114791.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
